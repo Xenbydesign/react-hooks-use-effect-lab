de351d101b1a46635b361439f620d82208e0f1ca
Xennia Contreras <Xenbydesign@gmail.com>
2024-02-26 21:30:24 -0800

    completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0){
+      setTimeRemaining(10);
+      onAnswered(false)
+    return
+    }
+    const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining -1)
+    },1000)
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+    return () => clearTimeout(timer)
+}, [timeRemaining, onAnswered]);
 
-  const { id, prompt, answers, correctIndex } = question;
+function handleAnswer(isCorrect) {
+  setTimeRemaining(10);
+  onAnswered(isCorrect);
+}; 
 
+const { id, prompt, answers, correctIndex } = question;
+  
   return (
     <>
       <h1>Question {id}</h1>
